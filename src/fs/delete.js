ec6d749dc639cf7c199eebd97582c2c9d0979821
Ekaterina Kiryanova <109188496+KrKate@users.noreply.github.com>
2024-01-29 16:25:38 +0500

    feat: add fs solution

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,17 @@
+import { unlink } from "fs/promises";
+import { dirname, join } from "path";
+import { fileURLToPath } from 'url';
+
 const remove = async () => {
-    // Write your code here 
+  try {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const filePath = join(__dirname, "files", "fileToRemove.txt");
+    await unlink(filePath);
+    console.log("File removed successfully");
+  } catch (error) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await remove();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,24 @@
+import { promises as fs, constants } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const rename = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const filePath = path.join(__dirname, "files", 'wrongFilename.txt');
+    const renamefilePath = path.join(__dirname, "files", 'properFilename.md');
+  try {
+    await fs.access(filePath, constants.F_OK);
+    try {
+      await fs.access(renamefilePath, constants.F_OK);
+      throw new Error('FS operation failed');
+    } catch (error) {
+      await fs.rename(filePath, renamefilePath);
+      console.log('File renamed successfully');
+    }
+  } catch (error) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await rename();
